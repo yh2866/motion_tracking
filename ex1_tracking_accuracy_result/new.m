4be310b5621891e202bbb398dbbd5b4728ade656
Yuanqing Hong <yh2866@columbia.edu>
2017-07-04 14:58:03 -0400

    before adding sphere

--- ex1_tracking_accuracy_result/new.m
+++ ex1_tracking_accuracy_result/new.m
@@ -1,10 +1,28 @@
 [wandO1,wandO2,wandO3,wandX1,wandX2,wandX3,wandX4,wandX5,wandX6,wandY11,wandY12,wandY13,wandY21,wandY22,wandY23,Tracker1,Tracker2,Tracker3,Tracker4,Tracker5,Tracker6,Tracker7] = importfile('test_rotation.txt', 3, inf);
+%Preprocessing: get rid of missing points
+for i = 1:length(wandO1)
+    if (wandO1(i) == 0 || wandO2(i) == 0 || wandO3(i) == 0) ...
+        || (wandX1(i) == 0 || wandX1(i) == 0 || wandX1(i) == 0) ...
+        || (wandY11(i) == 0 || wandY12(i) == 0 || wandY13(i) == 0)
+        i
+        wandO1(i) = wandO1(i-1);
+        wandO2(i) = wandO2(i-1);
+        wandO3(i) = wandO3(i-1);
+        wandX1(i) = wandX1(i-1);
+        wandX2(i) = wandX2(i-1);
+        wandX3(i) = wandX3(i-1);
+        wandY11(i) = wandY11(i-1);
+        wandY12(i) = wandY12(i-1);
+        wandY13(i) = wandY13(i-1);
+    end
+end
+
 GlobalCoord = [1,0,0,0;
                0,1,0,0;
                0,0,1,0;
                0,0,0,1];
 Track = zeros(length(wandO1),4);
-for i = 1:length(wandO1)     
+for i = 1:length(wandO1)
     wand_o = [wandO1(i),wandO2(i),wandO3(i)];
     wand_x = [wandX1(i),wandX2(i),wandX3(i)];
     wand_y = [wandY11(i),wandY12(i),wandY13(i)];
@@ -15,9 +33,13 @@ for i = 1:length(wandO1)
     z_norm = cross(x_norm,y_norm);
 
     M = ([x_norm,0;y_norm,0;z_norm,0;wand_o,1]/GlobalCoord)';
-
-    Track(i,:) = M*[x_mod/2,y_mod/2,0,1]';
+    %The real distance:95mm 70mm 37mm
+    Track(i,:) = M*[95,70,-30,1]';
 end
+
+% Tracker11 = Tracker1(1:round(length(Tracker1)/2));
+% Tracker22 = Tracker2(1:round(length(Tracker2)/2));
+% Tracker33 = Tracker3(1:round(length(Tracker3)/2));
 
 Tracker2 = -Tracker2;
 
@@ -32,4 +54,9 @@ plot3(Y(:,1),Y(:,2),Y(:,3),'b');
 hold on
 plot3(Z(:,1),Z(:,2),Z(:,3),'g');
 hold on
-error = [mean(abs((X(:,1)-Z(:,1)))),mean(abs((X(:,2)-Z(:,2)))),mean(abs((X(:,3)-Z(:,3))))]
+Error = X - Z;
+error_abs = [mean(abs(X(:,1)-Z(:,1))),mean(abs(X(:,2)-Z(:,2))),mean(abs(X(:,3)-Z(:,3)))]
+error_rms = [rms(Error(:,1)),rms(Error(:,2)),rms(Error(:,3))]
+error_max = [max(Error(:,1)),max(Error(:,2)),max(Error(:,3))]
+
+
